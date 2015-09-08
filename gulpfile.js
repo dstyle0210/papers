97abe5e74952b3af5d9388f502a809247e1ac286
dstyle0210 <dstyle0210@gmail.com>
2015-09-08 16:36:37 +0900

    prototype init.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 var csso = require('gulp-csso');
 
 gulp.task('sass', function () {
-    gulp.src('./src/sass/*.scss')
+    gulp.src('./src/sass/papers.scss')
         .pipe(sass().on('error', sass.logError)) // SCSS -> CSS
         .pipe(csso()) // CSS minify
         .pipe(gulp.dest('./src/css'));
